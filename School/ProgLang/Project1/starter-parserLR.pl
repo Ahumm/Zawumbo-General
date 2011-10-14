66ae4f0876efbe8586b4e184602b9484924bd188
Tate Larsen <larset2@gmail.com>
2011-10-14 19:19:26 -0400

    More Prolog.

--- School/ProgLang/Project1/starter-parserLR.pl
+++ School/ProgLang/Project1/starter-parserLR.pl
@@ -1,9 +1,16 @@
 % Representation for production is analogous to that
 % in Problem 1. 
 
-prod(1,[non(e,_),non(e,_),term(minus,_),non(t,_)]).
+input0([3,-,5]).
+input1([3,-,5,*,7,-,18]).
+input2([1,-,5,*,7,*,2,-,10]).
+input3([1,+,9]).
+input4([100,-,1,*,2,*,3,-4,*,5,-,6,-,7,*,10,*,11,*,12]).
+input5([100,-,1,*,2,*,3,-,4,*,5,-,6,-,7,*,10,*,11,*,12]).
+
+prod(1,[non(e,_),non(t,_),term(minus,_),non(e,_)]).
 prod(2,[non(e,_),non(t,_)]).
-prod(3,[non(t,_),non(t,_),term(mul,_),term(num,_)]).
+prod(3,[non(t,_),term(num,_),term(mul,_),non(t,_)]).
 prod(4,[non(t,_),term(num,_)]).
 
 % YOUR CODE HERE.
@@ -16,27 +23,29 @@ prod(4,[non(t,_),term(num,_)]).
 % and nonterminal E, the parser moves to state 1. 
 
 action(0,term(num,_),shift,3).
+%
 action(1,term(minus,_),shift,4).
-%%%%%%%%%%%%%%%%%%%%%%%action(1,term(end,_),
-action(2,term(minus,_),reduce,2)
-action(2,term(mul,_),shift,5)
-action(2,term(end,_),reduce,2)
+action(1,term(end,_),accept,0).
 %
-action(3,term(minus,_),reduce,4)
-action(3,term(mul,_),reduce,4)
-action(3,term(end,_),reduce,4)
+action(2,term(minus,_),reduce,2).
+action(2,term(mul,_),shift,5).
+action(2,term(end,_),reduce,2).
 %
-action(4,term(num,_),shift,3)
+action(3,term(minus,_),reduce,4).
+action(3,term(mul,_),reduce,4).
+action(3,term(end,_),reduce,4).
 %
-action(5,term(num,_),shift,5)
+action(4,term(num,_),shift,3).
 %
-action(6,term(minus,_),reduce,1)
-action(6,term(mul,_),shift,5)
-action(6,term(end,_),reduce,1)
+action(5,term(num,_),shift,7).
 %
-action(7,term(minus,_),reduce,3)
-action(7,term(mul,_),reduce,3)
-action(7,term(end,_),reduce,3)
+action(6,term(minus,_),reduce,1).
+action(6,term(mul,_),shift,5).
+action(6,term(end,_),reduce,1).
+%
+action(7,term(minus,_),reduce,3).
+action(7,term(mul,_),reduce,3).
+action(7,term(end,_),reduce,3).
 
 goto(0,non(e,_),1).
 goto(0,non(t,_),2).
@@ -46,10 +55,10 @@ goto(4,non(t,_),6).
 % Complete the attribute table. Predicates attribute() encode the
 % extension of the grammar which computes the value of the expression.
 
-attribute(1,[non(e,A),non(e,A1),term(minus,_),non(t,A2)]):- % YOUR CODE HERE
-attribute(2,[non(e,A),non(t,A1)]).
-attribute(3,[non(t,A),non(t,A1),term(mul,_),term(num,A2)]).
-attribute(4,[non(t,A),term(num,A1)]).
+attribute(1,[non(e,A),non(e,A1),term(minus,_),non(t,A2)]):- A is A1 - A2.
+attribute(2,[non(e,A),non(t,A1)]) :- A is A1.
+attribute(3,[non(t,A),non(t,A1),term(mul,_),term(num,A2)]) :- A is A1 * A2.
+attribute(4,[non(t,A),term(num,A1)]) :- A is A1.
 
 % YOUR CODE HERE.
 % As with the LL(1) parser, begin with transform(L,R).
@@ -66,12 +75,19 @@ transform([H|Tail],R) :- transform(Tail, S),H=[],append([term(eps,_)],S,R).
 % input0(L),parseLR(L,ProdSeq, V).
 % ProdSeq = [1, 4, 2, 4]
 % V = -2.
-parseLR(List,ProdSeq, V) :- transform(List, TransformedList), workThrough(TransformedList, [non(e,_)], ProdSeq).
+parseLR(List,ProdSeq, V) :- transform(List,TransformedList), workThrough(TransformedList,[],[0],ProdSeq).
 
-workThrough([IHead|ITail], [IHead|PTail], R) :- workThrough(ITail,PTail,R).
-workThrough(IList, [term(eps,_)|PTail], R) :- workThrough(IList,PTail,R).
-workThrough([IHead|ITail], [PHead|PTail], R) :- expand(PHead,IHead,Prod), prod(Prod,[_|NPTail]), append(NPTail, PTail, NStack), workThrough([IHead|ITail], NStack, X), append([Prod],X, R).
-workThrough([term(end,_)|_], [], []).
+workThrough([IHead|ITail],Stack,[HState|TState],Prod) :- action(HState,IHead,shift,NState),append([IHead],Stack,NStack),append([NState],[HState|TState], NSS),workThrough(ITail,NStack,NSS,Prod).
+
+workThrough([IHead|ITail],Stack,[HState|TState],Prod) :- action(HState,IHead,reduce,Reduct),prod(Reduct, [PResult|PMatch]), cull(Stack,[HState|TState],PMatch,SR,StateB),append([PResult],SR,NStack),gotoState([IHead|ITail],NStack,StateB,NProd),append(NProd,[Reduct],Prod).
+
+cull([HStack|TStack],[HState|TState],[HStack|TProdMatch],RStack,RState) :- cull(TStack,TState,TProdMatch,RStack,RState).
+
+cull(RStack,RState, [], RStack,RState).
+
+gotoState(I, [HStack|TStack],[HState|TState],Prod) :- goto(HState,HStack,Dest),append([Dest],[HState|TState],NState),workThrough(I,[HStack|TStack],NState,Prod).
+
+gotoState([term(end,_)],[non(e,_)],State,Prod).
 
 
 
